3ee19c32dd4d4d710ae62f8d96007e457fe80f64
actionchain <actionchain@chainguard.dev>
2026-06-03 14:34:28 +0000

    Update hashicorp/setup-terraform@v4.0.1

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright IBM Corp. 2020, 2026
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+const core = require('@actions/core');
+
+const setup = require('./lib/setup-terraform');
+
+(async () => {
+  try {
+    await setup();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
